2a36df33e6457c2f0bcd302809e82ea62c8a6724
Jonathan Bramble <j.p.bramble@leeds.ac.uk>
2013-07-23 09:38:43 +0100

    Update GPL

--- src/spr_wrap.cpp
+++ src/spr_wrap.cpp
@@ -1,3 +1,21 @@
+/*
+ * spr_wrap.ccp
+ * Copyright (C) Jonathan Bramble 2011
+ * 
+FBF-Optics is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * FBF-Optics is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <Rcpp.h>
 #include "spr.hpp"
 //using namespace Rcpp;
